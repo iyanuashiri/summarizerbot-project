8fce53907d5790dbd0493d4d69ef3440a6682ef7
Iyanuoluwa Ajao <ajaoiyanu@gmail.com>
2023-01-23 00:07:35 +0100

    feat: add create_summaries lambda function

---
+++ create_summaries/summaries.py
@@ -0,0 +1,29 @@
+import json
+import asyncio
+
+import aiohttp
+from decouple import config
+
+
+POST_URL = config("POST_URL")
+
+
+async def create_summaries(title, summary, url, session):
+    payload = {'title': title, 'summary': summary, 'url': url}
+    async with session.post(f"{POST_URL}", data=payload) as response:
+        result = await response.text()
+        return result
+
+
+async def main(event):
+    details = json.loads(event)
+    details = details['detail']['detail']
+    async with aiohttp.ClientSession() as session:
+        tasks = [create_summaries(title=detail['title'], summary=detail['summary'], url=detail['url'], session=session)
+                 for detail in details]
+        list_of_tasks = await asyncio.gather(*tasks)
+        return list_of_tasks
+
+
+def lambda_handler(event, context):
+    asyncio.run(main(event=event))
