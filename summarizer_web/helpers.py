32c30afb2558bfb419bbfb5de1cb15f98c57ebd0
Iyanuoluwa Ajao <ajaoiyanu@gmail.com>
2023-01-23 13:23:38 +0100

    refactor: remove unused code

--- summarizer_web/helpers.py
+++ summarizer_web/helpers.py
@@ -2,18 +2,8 @@ from summarizer_web.app import create_app
 from summarizer_web.app.models import Summary
 from summarizer_web.app import db
 
-from goose3 import Goose
-
-
 app = create_app()
 app.app_context().push()
-
-
-def get_title(url):
-    g = Goose()
-    article = g.extract(url=url)
-    title = article.title
-    return title
 
 
 def create_summary(title, summary, url):
@@ -21,5 +11,3 @@ def create_summary(title, summary, url):
     db.session.add(s)
     db.session.commit()
     return s
-
-
