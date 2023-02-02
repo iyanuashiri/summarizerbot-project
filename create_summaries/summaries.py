46ccd70c05535616c59f7cb4e3075902235bc6cd
Iyanuoluwa Ajao <ajaoiyanu@gmail.com>
2023-02-02 12:53:33 +0100

    add uuid field to create_summaries

--- create_summaries/summaries.py
+++ create_summaries/summaries.py
@@ -8,8 +8,8 @@ from decouple import config
 POST_URL = config("POST_URL")
 
 
-async def create_summaries(title, summary, url, session):
-    payload = {'title': title, 'summary': summary, 'url': url}
+async def create_summaries(title, summary, url, uuid, session):
+    payload = {'title': title, 'summary': summary, 'url': url, 'uuid': uuid}
     async with session.post(f"{POST_URL}", data=payload) as response:
         result = await response.text()
         return result
@@ -19,7 +19,8 @@ async def main(event):
     response_payload = event['responsePayload']
     details = json.loads(response_payload)
     async with aiohttp.ClientSession() as session:
-        tasks = [create_summaries(title=detail['title'], summary=detail['summary'], url=detail['url'], session=session)
+        tasks = [create_summaries(title=detail['title'], summary=detail['summary'], url=detail['url'],
+                                  uuid=detail['uuid'], session=session)
                  for detail in details]
         list_of_tasks = await asyncio.gather(*tasks)
         return list_of_tasks
